f6645660c90e317d8b482ebe8d8a78da0d3a7685
Sam Kilgus <samkilgus@gmail.com>
2018-02-22 20:52:41 -0500

    adding mobile submit message; remove scroll title skews on mobile; improved home autodraw behaviour

--- public/main.js
+++ public/main.js
@@ -8,6 +8,8 @@ var menuCharWidth;
 var updateScroll = true;
 var sectionHeight;
 
+var isMobile;
+
 /******************************************************************************/
 /******************************* HELPERS **************************************/
 /******************************************************************************/
@@ -58,6 +60,12 @@ function elementScrolledIntoView(el) {
 
     return (top + height > 0) && (top < window.innerHeight);
 }
+
+function chechIfMobile() {
+    var check = false;
+    (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) check = true;})(navigator.userAgent||navigator.vendor||window.opera);
+    return check;
+};
 
 /******************************************************************************/
 /******************************* MENU *****************************************/
@@ -202,10 +210,11 @@ function sectionScrollHandler(ev) {
         lastScrollTop = this.scrollTop;
     }
 
-    curTitleSkew = Math.max(-MAX_TITLE_SKEW,Math.min(MAX_TITLE_SKEW,curTitleSkew+(scrollDiff/30)));
-
-    if (!updateTitleSkewsInterval)
-        updateTitleSkewsInterval = setInterval(updateTitleSkews, 10);
+    if (!isMobile) {
+        curTitleSkew = Math.max(-MAX_TITLE_SKEW,Math.min(MAX_TITLE_SKEW,curTitleSkew+(scrollDiff/30)));
+        if (!updateTitleSkewsInterval)
+            updateTitleSkewsInterval = setInterval(updateTitleSkews, 10);
+    }
 
     if (elementScrolledIntoView(document.getElementById("home_sec")) && !homeAutoDraw)
         startHomeDraw();
@@ -240,20 +249,21 @@ function initSections() {
 /******************************* HOME SECTION DRAWER **************************/
 /******************************************************************************/
 
-const MAX_HOME_DRAW_SPEED = 50;
+const MAX_HOME_DRAW_SPEED = 30;
 const MAX_HOME_DRAW_VECT = 50;
-const MAX_HOME_DRAW_VECT_CHANGE = 10;
+const MAX_HOME_DRAW_ACC = 20;
+const MAX_HOME_DRAW_ACC_CHANGE = 1;
 const HOME_DRAW_BOUND_PERC = 4;
 
 var homeAutoDraw;
 
 var homeDrawTimeout;
+var curAcc;
 var curVect;
 var curPos;
 var curSpeed = 10;
 
 function homeDrawStep() {
-    // console.log("homeDrawStep");
     var steerXL = Math.max(0,Math.min(1, (curPos[0]/(homeCanv.width/HOME_DRAW_BOUND_PERC))));
     var steerXR = Math.max(0,Math.min(1, (Math.abs(homeCanv.width - curPos[0])/(homeCanv.width/HOME_DRAW_BOUND_PERC))));
     var steerYT = Math.max(0,Math.min(1, (curPos[1]/(homeCanv.height/HOME_DRAW_BOUND_PERC))));
@@ -261,10 +271,17 @@ function homeDrawStep() {
 
     // console.log(steerXL.toFixed(2),steerXR.toFixed(2),steerYT.toFixed(2),steerYB.toFixed(2));
 
-    curVect[0] = Math.max(-MAX_HOME_DRAW_VECT,Math.min(MAX_HOME_DRAW_VECT,curVect[0] +
-                  randFloat(-MAX_HOME_DRAW_VECT_CHANGE*steerXL,MAX_HOME_DRAW_VECT_CHANGE*steerXR)));
-    curVect[1] = Math.max(-MAX_HOME_DRAW_VECT,Math.min(MAX_HOME_DRAW_VECT,curVect[1] +
-                  randFloat(-MAX_HOME_DRAW_VECT_CHANGE*steerYT,MAX_HOME_DRAW_VECT_CHANGE*steerYB)));
+    curAcc[0] = Math.max(0,Math.min(MAX_HOME_DRAW_ACC,
+        curAcc[0] + randFloat(-MAX_HOME_DRAW_ACC_CHANGE,MAX_HOME_DRAW_ACC_CHANGE)));
+    curAcc[1] = Math.max(0,Math.min(MAX_HOME_DRAW_ACC,
+        curAcc[1] + randFloat(-MAX_HOME_DRAW_ACC_CHANGE,MAX_HOME_DRAW_ACC_CHANGE)));
+
+    // console.log(curAcc);
+
+    curVect[0] = Math.max(-MAX_HOME_DRAW_VECT,Math.min(MAX_HOME_DRAW_VECT,
+        curVect[0] + randFloat(-curAcc[0]*steerXL,curAcc[0]*steerXR)));
+    curVect[1] = Math.max(-MAX_HOME_DRAW_VECT,Math.min(MAX_HOME_DRAW_VECT,
+        curVect[1] + randFloat(-curAcc[1]*steerYT,curAcc[1]*steerYB)));
 
     var dist = Math.pow((curVect[0] * curVect[0]) + (curVect[1] * curVect[1]),0.5);
 
@@ -280,8 +297,9 @@ function homeDrawStep() {
 }
 
 function startHomeDraw() {
-    curVect  = [randFloat(-20,20),randFloat(-20,20)];
-    curPos   = [homeCanv.width/2,homeCanv.height/2];
+    curAcc  = [randFloat(-10,10),randFloat(-10,10)];
+    curVect = [randFloat(-20,20),randFloat(-20,20)];
+    curPos  = [homeCanv.width/2,homeCanv.height/2];
     homeAutoDraw = true;
     if (homeDrawTimeout) clearInterval(homeDrawTimeout);
     homeDrawTimeout = setTimeout(homeDrawStep, curSpeed);
@@ -300,7 +318,7 @@ var homeCanvUpdateInterval;
 var homeLastMX;
 var homeLastMY;
 
-var curMainDrawEls = [];
+var curMainDrawEls;
 
 function clearHomeCanv() {
     homeCtx.clearRect(0,0,homeCanv.width,homeCanv.height);
@@ -320,11 +338,11 @@ function newHomeChar(x,y,dist) {
 }
 
 function updateMainCanv() {
-    window.requestAnimationFrame(function functionName() {
+    window.requestAnimationFrame(function () {
         if (curMainDrawEls.length > 20) {
             if (curSpeed) {
                 if (chance.bool({likelihood:Math.max(0,Math.min(100,100-curSpeed))}))
-                    curMainDrawEls.splice(0,Math.max(1,Math.round(Math.pow(curMainDrawEls.length,0.35))));
+                    curMainDrawEls.splice(0,Math.max(1,Math.round(Math.pow(curMainDrawEls.length,0.5))));
             }
             else
                 curMainDrawEls.splice(0,Math.max(1,Math.round(Math.pow(curMainDrawEls.length,0.35))));
@@ -384,6 +402,7 @@ function initHomeCanv() {
 }
 
 function initHomeSection() {
+    curMainDrawEls = [];
     homeSection = document.getElementById("home_sec");
     initHomeCanv();
     homeCanv.addEventListener("mousemove",homeCanvMouseMoveListener);
@@ -628,10 +647,14 @@ function initSubmitElements() {
 }
 
 function initSubmit() {
-    initSubmitElements();
-    submitButton = document.getElementById("centerSE");
-    submitButton.addEventListener("mousedown",submitClickHandler);
-    submitButton.addEventListener("mouseup",submitCancelHandler);
+    if (!isMobile) {
+        document.getElementById("submit_sec").classList.remove("mobile");
+        document.getElementById("submit_sec").innerHTML = null;
+        initSubmitElements();
+        submitButton = document.getElementById("centerSE");
+        submitButton.addEventListener("mousedown",submitClickHandler);
+        submitButton.addEventListener("mouseup",submitCancelHandler);
+    }
 }
 
 /******************************************************************************/
@@ -705,6 +728,7 @@ function resize() {
 /******************************************************************************/
 
 function init() {
+    isMobile = chechIfMobile();
     startLoad();
     grainOverlay.init();
     initMenu();
@@ -713,8 +737,8 @@ function init() {
     initAboutGLPH();
     initDigLit();
     initSubmit();
-    // setTimeout(endLoad, randInt(1000,2000));
-    endLoad();
+    setTimeout(endLoad, randInt(1000,2000));
+    // endLoad();
 }
 
 window.onload = init;
